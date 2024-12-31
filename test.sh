c3fdc2357d1305414c067ba557efb3afd06f45ba
JPFranca <135433891+jpfranca-br@users.noreply.github.com>
2024-12-31 20:06:01 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Publish a message to the MQTT topic with a dynamically generated ID and message
 mosquitto_pub -h broker.hivemq.com -p 1883 -t printerserver -m '{
   "id": "'$(date +%s)'",
   "message": "Hello, world! Test message ID '$(date +%s)'.",
