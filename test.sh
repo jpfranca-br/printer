3af7610b6750340cc65733a4cc09059bac27b066
JPFranca <135433891+jpfranca-br@users.noreply.github.com>
2024-12-31 20:11:45 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# Publish a message to the MQTT topic with a dynamically generated ID and message
+
+# Generate a unique ID based on the current epoch time
+message_id=$(date +%s)
+
+# Publish a message to the MQTT topic with the generated ID
 mosquitto_pub -h broker.hivemq.com -p 1883 -t printerserver -m '{
-  "id": "'$(date +%s)'",
-  "message": "Hello, world! Test message ID '$(date +%s)'.",
+  "id": "'"$message_id"'",
+  "message": "Hello, world! Test message ID '"$message_id"'.",
   "callback": "https://apimocha.com/printerserver/callback"
 }'
+
+# Echo the message ID
+echo "Message sent with ID: $message_id"
